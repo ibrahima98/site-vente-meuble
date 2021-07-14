ad79b911135711463ce8aa98966c236e3ab4f96d
abdoul wahab ly <abdoulwahably71@gmail.com>
2021-07-14 23:35:15 +0000

    mise  en page du homepage

---
+++ homepage.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <p class="logo">Logo</p>
+        <ul class="link">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Accueil</a></li>
+        </ul>
+    </header>
+    <div class="section-1">
+        <div class="presentstion-aside"></div>
+        <div class="presentation-content"></div>
+    </div>
+
+    <div class="section-2">
+        <div class="card">
+            <div class="card-ctn">performance</div>
+            <div class="card-ctn">performance</div>
+            <div class="card-ctn">performance</div>
+            <div class="card-ctn">performance</div>
+        </div>
+        <div>
+            <h1>Dernieres produits</h1>
+            <div class="last-produit">
+                <div class="last-produit-ctn">produit</div>
+                <div class="last-produit-ctn">produit</div>
+                <div class="last-produit-ctn">produit</div>
+                <div class="last-produit-ctn">produit</div>
+                <div class="last-produit-ctn">produit</div>
+                <div class="last-produit-ctn">produit</div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
