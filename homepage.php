3bbf407a7af4e4d254ca482ca811145756ae265b
abdoul wahab ly <abdoulwahably71@gmail.com>
2021-07-16 21:55:03 +0000

    ,modif

--- homepage.php
+++ homepage.php
@@ -11,7 +11,7 @@
 
 <body>
     <header>
-        <p class="logo">Logo</p>
+        <p class="logo"><img src="./public/imgS/monLogo (1).jfif"></p>
         <ul class="link">
             <li><a href="#">Accueil</a></li>
             <li><a href="#">Boutique</a></li>
@@ -21,7 +21,7 @@
     </header>
     <div class="section-1">
         <div class="presentstion-aside">
-            <h1 class="section-1-h1">WEBSITE</h1>
+            <h1 class="section-1-h1">LYshop</h1>
             <p>Lorem Ipsum is simply dummy text of the printing<br>
                 and typesetting industry. Lorem Ipsum has been the<br>
                 industry's standard dummy text ever since the 1500s,<br>
@@ -36,19 +36,51 @@
     </div>
 
     <div class="section-2">
-        <div class="card">
-            <div class="card-ctn">performance</div>
-            <div class="card-ctn">performance</div>
-            <div class="card-ctn">performance</div>
-            <div class="card-ctn">performance</div>
+        <div class="cards">
+            <div class="card-ctn">
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="check-circle" class="svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="#7EEB3F" d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+                </svg>
+                <h4>Sure</h4>
+                <p>A Zshop nous vous vendons<br>
+                    des produits sure<br>
+                    avec garantie 5 ans</p>
+            </div>
+            <div class="card-ctn">
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="check-circle" class="svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="#7EEB3F" d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+                </svg>
+                <h4>Sure</h4>
+                <p>A Zshop nous vous vendons<br>
+                    des produits sure<br>
+                    avec garantie 5 ans</p>
+            </div>
+            <div class="card-ctn">
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="check-circle" class="svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="#7EEB3F" d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+                </svg>
+                <h4>Sure</h4>
+                <p>A Zshop nous vous vendons<br>
+                    des produits sure<br>
+                    avec garantie 5 ans</p>
+            </div>
+            <div class="card-ctn">
+                <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="check-circle" class="svg-inline--fa fa-check-circle fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path fill="#7EEB3F" d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 48c110.532 0 200 89.451 200 200 0 110.532-89.451 200-200 200-110.532 0-200-89.451-200-200 0-110.532 89.451-200 200-200m140.204 130.267l-22.536-22.718c-4.667-4.705-12.265-4.736-16.97-.068L215.346 303.697l-59.792-60.277c-4.667-4.705-12.265-4.736-16.97-.069l-22.719 22.536c-4.705 4.667-4.736 12.265-.068 16.971l90.781 91.516c4.667 4.705 12.265 4.736 16.97.068l172.589-171.204c4.704-4.668 4.734-12.266.067-16.971z"></path>
+                </svg>
+                <h4>Sure</h4>
+                <p>A Zshop nous vous vendons<br>
+                    des produits sure<br>
+                    avec garantie 5 ans</p>
+            </div>
+
         </div>
-        <div>
-            <h1>Dernieres produits</h1>
+        <div class="last">
+            <h1 class="last-title">Dernieres produits</h1>
             <div class="last-produit">
-                <div class="last-produit-ctn">produit</div>
-                <div class="last-produit-ctn">produit</div>
-                <div class="last-produit-ctn">produit</div>
-                <div class="last-produit-ctn">produit</div>
+                <div class="last-produit-ctn">
+                    <img src="./public/imgS/">
+                </div>
                 <div class="last-produit-ctn">produit</div>
                 <div class="last-produit-ctn">produit</div>
 
