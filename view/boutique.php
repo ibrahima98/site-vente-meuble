3e41e3a21b84d51f9c4538022380a06c3af2d356
abdoul wahab ly <abdoulwahably71@gmail.com>
2021-07-18 10:46:45 +0000

    homepage presque termine

--- view/boutique.php
+++ view/boutique.php
@@ -1,4 +1,4 @@
-<?php require_once "../header.php"; ?>
+<?php require_once "./../header.php"; ?>
 
 <h1>Categorie :</h1>
 <div class="categories-link">
@@ -13,4 +13,4 @@
 
 
 
-<?php require_once "../footer.php"; ?>
+<?php require_once "./../footer.php"; ?>
