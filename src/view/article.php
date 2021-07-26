08c7d882d675bbddd84149ee82ae7465fe16dbf9
abdoul wahab ly <abdoulwahably71@gmail.com>
2021-07-26 19:25:41 +0000

    pade d'un article

---
+++ src/view/article.php
@@ -0,0 +1,50 @@
+<?php require_once "header.php"; ?>
+
+
+<section class="article-section">
+
+    <div class="aside-article">
+        <p>Produits Similaire</p>
+        <div class="aside-article-ctn">
+            <div>
+                <img src="./../public/imgS/sofa.jpg" class="img-smaller">
+                <h4>Sofa Maron</h4>
+            </div>
+            <div>
+                <img src="./../public/imgS/sofa.jpg" class="img-smaller">
+                <h4>Sofa Maron</h4>
+            </div>
+            <div>
+                <img src="./../public/imgS/sofa.jpg" class="img-smaller">
+                <h4>Sofa Maron</h4>
+            </div>
+            <div>
+                <img src="./../public/imgS/sofa.jpg" class="img-smaller">
+                <h4>Sofa Maron</h4>
+            </div>
+
+
+        </div>
+
+    </div>
+    <div class="ctn-article">
+        <div>
+            <img src="./../public/imgS/sofa.jpg" class="img-larger">
+            <h1>Sofa Rouge</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing<br>
+                and typesetting industry. Lorem Ipsum has been the </p>
+            <p class="prix">22 000 FCFA</p>
+        </div>
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php require_once "footer.php"; ?>
