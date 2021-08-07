5610d1d7a0629fb0195e9c4ab21e3d3afd6054f6
abdoul wahab ly <abdoulwahably71@gmail.com>
2021-08-07 11:29:09 +0000

    Lien sur image dans boutique

--- src/view/boutique.php
+++ src/view/boutique.php
@@ -12,20 +12,20 @@
     </ul>
 </div>
 <div class="boutique">
-<?php 
-   require "../../src/controller/control.php";
-    foreach ($contain as  $produit):
-        ?> 
-          
-                <div class="boutique-ctn">
-                <img src="../../src/model/classes/<?=$produit->image?>">
-                     <div class="proprites">
-                          <h4><?=ucwords($produit->nom);?></h4>
-                          <p>Price:<?=number_format($produit->prix,2,',','');?> £</p>
-                     </div>
-                 </div>
-           
-     <?php endforeach?>
-     </div>
+    <?php
+    require "../../src/controller/control.php";
+    foreach ($contain as  $produit) :
+    ?>
+
+        <div class="boutique-ctn">
+            <a href="article.php?id=<?php echo $produit->id_produit ?>"><img src="../../src/model/classes/<?= $produit->image ?>"></a>
+            <div class="proprites">
+                <h4><?= ucwords($produit->nom); ?></h4>
+                <p>Price:<?= number_format($produit->prix, 2, ',', ''); ?> £</p>
+            </div>
+        </div>
+
+    <?php endforeach ?>
+</div>
 
 <?php require_once "footer.php"; ?>
